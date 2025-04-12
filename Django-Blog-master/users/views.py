6f9d4ba4da750a761e4196f6d6a9e30dddb086e5
Varshu99 <72430323+Varshu99@users.noreply.github.com>
2025-04-12 09:26:10 +0000

    pages

--- Django-Blog-master/users/views.py
+++ Django-Blog-master/users/views.py
@@ -11,7 +11,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created! You are now able to log in')
-            return redirect('signin')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
@@ -42,6 +42,3 @@ def profile(request):
     return render(request, 'users/profile.html', context)
 
 
-def logout(req):
-    logout(req)
-    return redirect("home")
